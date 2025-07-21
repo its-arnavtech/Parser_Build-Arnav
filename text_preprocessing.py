1a5b7a6defc6da495bd1cb0056af6f6de2839ab9
its-arnavtech <arnavk174@gmail.com>
2025-07-21 13:00:46 -0500

    sucessfully filtered stop words in preprocessing

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -1,14 +1,32 @@
 import spacy
 import nltk
-import re
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from nltk import pos_tag, ne_chunk
+from pdfminer.high_level import extract_text
+import sys
+
+sample_resume_text = sys.stdout.buffer.write(extract_text(r'C:\Flexon_Resume_Parser\Parser_Build-Arnav\Resume_ArnavK.pdf').encode('utf-8'))
 
 nlp = spacy.load("en_core_web_md")
 
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('words')
-nltk.download('averaged_perception_tagger')
-nltk.download('maxent_ne_chunker')
+#nltk.download('punkt')
+#nltk.download('stopwords')
+#nltk.download('words')
+#nltk.download('averaged_perception_tagger')
+#nltk.download('maxent_ne_chunker')
+
+def preprocessing_text(text):
+    text = text.lower() #lowercase to avoid issues and treat all words equal
+    tokens = word_tokenize(text) #tokenization
+    stop_words = set(stopwords.words("english"))
+
+    filtered_tokens = []
+    for word in tokens:
+        if word.isalpha() and word not in stop_words:
+            filtered_tokens.append(word)
+
+    return filtered_tokens
+
+filtered_tokens = preprocessing_text(sample_resume_text)
+print(filtered_tokens)
