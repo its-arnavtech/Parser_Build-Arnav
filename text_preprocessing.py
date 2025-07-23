06efafb36d59539c34be35b99279cd2d8b4e7bf6
its-arnavtech <arnavk174@gmail.com>
2025-07-22 21:03:57 -0500

    extracting emails

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -4,9 +4,10 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from nltk import pos_tag, ne_chunk
 from pdfminer.high_level import extract_text
-import sys
+#import sys
+from spacy.matcher import Matcher
 
-sample_resume_text = sys.stdout.buffer.write(extract_text(r'C:\Flexon_Resume_Parser\Parser_Build-Arnav\Resume_ArnavK.pdf').encode('utf-8'))
+sample_resume_text = extract_text(r'C:\Flexon_Resume_Parser\Parser_Build-Arnav\Resume_ArnavK.pdf').encode('utf-8')
 
 nlp = spacy.load("en_core_web_md")
 
@@ -17,6 +18,8 @@ nlp = spacy.load("en_core_web_md")
 #nltk.download('maxent_ne_chunker')
 
 def preprocessing_text(text):
+    if not isinstance(text, str):
+        text = str(text)
     text = text.lower() #lowercase to avoid issues and treat all words equal
     tokens = word_tokenize(text) #tokenization
     stop_words = set(stopwords.words("english"))
@@ -28,5 +31,19 @@ def preprocessing_text(text):
 
     return filtered_tokens
 
-filtered_tokens = preprocessing_text(sample_resume_text)
-print(filtered_tokens)
+#filtered_tokens = preprocessing_text(sample_resume_text)
+#print(filtered_tokens)
+
+#def extract_name:
+
+def extract_email(text):
+    doc = nlp(text)
+    emails = []
+
+    for ent in doc.ents:
+        if ent.label_ == "EMAIL":
+            emails.append(ent.text)
+
+    print(emails)
+
+extract_email(sample_resume_text)
