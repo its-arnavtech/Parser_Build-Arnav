3a5e91355271b2dab23ebec89c2fbb54b40e3fa4
its-arnavtech <arnavk174@gmail.com>
2025-08-03 17:38:19 -0500

    Checking ORG label in resume

--- sample.py
+++ sample.py
@@ -1,5 +1,3 @@
-'''This file is only to see what data the pdf miner extracts from the pdf file'''
-
 import os
 import nltk
 from pdfminer.high_level import extract_text
@@ -9,7 +7,7 @@ import spacy
 
 # Load the SpaCy model
 nlp = spacy.load("en_core_web_md")
-docx = 'C:/Flexon_Resume_Parser/Parser_Build-Arnav/ATS classic HR resume.docx'
+
 def extract_text_from_docx(doc_path):
     try:
         doc = Document(doc_path)
@@ -21,28 +19,18 @@ def extract_text_from_docx(doc_path):
         print(f"Error extracting text: {e}")
         return None
 
+def work_experience(text):
+    doc = nlp(text)
+    print("Organizations found:")
+    for ent in doc.ents:
+        if ent.label_ == "ORG":
+            print(ent.text)
 
-def extract_text_from_pdf(pdf_path):
-    """Extracts text from a PDF file."""
-    try:
-        text = extract_text(pdf_path)
-        return text
-    except Exception as e:
-        print(f"Error extracting text: {e}")
-        return None
-
-# Path to the PDF file
-
+# Extract text from DOCX
 doc_resume_text = extract_text_from_docx('C:/Flexon_Resume_Parser/Parser_Build-Arnav/ATS classic HR resume.docx')
-if doc_resume_text:
-    if not isinstance(doc_resume_text, str):
-        pdf_resume_text = str(doc_resume_text)  # Ensure the text is a string
-    
-    # Tokenize the text into words
-    tokens = word_tokenize(doc_resume_text)
 
-    # Print each token
-    for token in tokens:
-        print(token)
+# Run only if text was successfully extracted
+if doc_resume_text:
+    work_experience(doc_resume_text)
 else:
-    print("No text extracted from the PDF.")
+    print("No text extracted from the resume.")
